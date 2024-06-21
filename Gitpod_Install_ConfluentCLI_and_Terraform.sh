052b7f8bb5ba372e3d0d53876de23c57237f2633
Phil Wild <83997558+pwildconfluentio@users.noreply.github.com>
2024-06-21 15:26:36 +0800

    Update Gitpod_Install_ConfluentCLI_and_Terraform.sh
    Typo correction.

--- Gitpod_Install_ConfluentCLI_and_Terraform.sh
+++ Gitpod_Install_ConfluentCLI_and_Terraform.sh
@@ -10,7 +10,6 @@ sudo chmod go+r /etc/apt/keyrings/confluent-cli.gpg
 echo "deb [signed-by=/etc/apt/keyrings/confluent-cli.gpg] https://packages.confluent.io/confluent-cli/deb stable main" | sudo tee /etc/apt/sources.list.d/confluent-cli.list >/dev/null
 sudo apt update
 sudo apt install confluent-cli
-sudo apt install confluent-cli
 
 echo "Installing Terraform
 
