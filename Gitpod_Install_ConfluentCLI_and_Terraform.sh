e67524fc3c774705d4d37df13f535606b32ec7e4
Phil Wild <83997558+pwildconfluentio@users.noreply.github.com>
2024-06-21 15:08:31 +0800

    Create Gitpod_Install_ConfluentCLI_and_Terraform.sh
    Add a script for use with Gitpod to setup the lab prerequisites

---
+++ Gitpod_Install_ConfluentCLI_and_Terraform.sh
@@ -0,0 +1,22 @@
+echo "Installing Confluent CLI
+
+------------------------------
+
+"
+sudo apt install curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl https://packages.confluent.io/confluent-cli/deb/archive.key | sudo gpg --dearmor -o /etc/apt/keyrings/confluent-cli.gpg
+sudo chmod go+r /etc/apt/keyrings/confluent-cli.gpg
+echo "deb [signed-by=/etc/apt/keyrings/confluent-cli.gpg] https://packages.confluent.io/confluent-cli/deb stable main" | sudo tee /etc/apt/sources.list.d/confluent-cli.list >/dev/null
+sudo apt update
+sudo apt install confluent-cli
+sudo apt install confluent-cli
+
+echo "Installing Terraform
+
+--------------------------
+
+"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
